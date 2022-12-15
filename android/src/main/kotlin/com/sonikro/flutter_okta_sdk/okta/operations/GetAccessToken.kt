3e22865b71b6d135294b4245feaa49de984bcc22
David Fournier <ceo@ipapps.dev>
2022-12-15 17:17:21 +0100

    Fix access token NPE

--- android/src/main/kotlin/com/sonikro/flutter_okta_sdk/okta/operations/GetAccessToken.kt
+++ android/src/main/kotlin/com/sonikro/flutter_okta_sdk/okta/operations/GetAccessToken.kt
@@ -5,7 +5,7 @@ import com.sonikro.flutter_okta_sdk.okta.entities.OktaClient
 import com.sonikro.flutter_okta_sdk.okta.entities.PendingOperation
 
 fun getAccessToken() {
-    OktaClient.getAuthClient().sessionClient.tokens.accessToken?.let {
+    OktaClient.getAuthClient().sessionClient?.tokens?.accessToken?.let {
         PendingOperation.success(it)
     } ?: PendingOperation.error(Errors.NO_ACCESS_TOKEN)
 }
